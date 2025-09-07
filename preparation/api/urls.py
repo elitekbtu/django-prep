0f434602744c033ed8a310120423b5d8b04a970c
elitekbtu <satbaldievst@mail.ru>
2025-09-08 01:26:17 +0500

    created views

--- preparation/api/urls.py
+++ preparation/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView
+from .views import BookListCreateView, BookDetailView, AuthorListCreateView, AuthorDetailView
 
 urlpatterns = [
     path("api/books/", BookListCreateView.as_view(), name="book-list-create"),
     path("api/books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("api/authors/", AuthorListCreateView.as_view(), name="author-list-create"),
+    path("api/authors/<int:pk>/", AuthorDetailView.as_view(), name="author-detail"),
 ]
--- preparation/api/views.py
+++ preparation/api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
 
 
 class BookListCreateView(APIView):
@@ -45,4 +45,43 @@ class BookDetailView(APIView):
     def delete(self, request, pk):
         book = get_object_or_404(Book, pk=pk)
         book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class AuthorListCreateView(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class AuthorDetailView(APIView):
+    def get(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        serializer = AuthorSerializer(author, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
